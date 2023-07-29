6115c43adc02ab702f99a9927266341d04363a51
Lucas Lessa <lucaslessa14@hotmail.com>
2023-07-29 01:27:59 -0300

    feat: create navbar element

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
