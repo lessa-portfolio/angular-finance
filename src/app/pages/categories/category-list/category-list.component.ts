2a4205a36dd8fcc4fa41d243cfdcbd017256dc26
Lucas Lessa <lucaslessa14@hotmail.com>
2023-08-02 23:00:55 -0300

    feat: list categories in categories page

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
+import { CategoryService } from './../shared/category.service';
 import { Component } from '@angular/core';
+import { Category } from '../shared/categories.model';
 
 @Component({
   selector: 'app-category-list',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-list.component.scss']
 })
 export class CategoryListComponent {
+  categories: Category[] = [];
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe({
+      next: categories => this.categories = categories,
+      error: error => console.error(error)
+    });
+  }
+
+  deleteCategory(category: Category) {
+    this.categoryService.delete(category.id).subscribe({
+      next: () => this.categories = this.categories.filter(el => el !== category),
+      error: error => console.error(error)
+    });
+  }
 }
